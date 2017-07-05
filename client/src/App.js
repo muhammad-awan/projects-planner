91db30c6ce0eb089d99e103d9310c3cd9de5e927
Muhammad Awan <muhammad.adam.awan@gmail.com>
2017-07-05 23:39:37 +1000

    Client: Add some react front end functionality

--- client/src/App.js
+++ client/src/App.js
@@ -1,20 +1,35 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+state = {
+  projects: null
+}
+
   render() {
+    const { projects } = this.state
+
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      {
+       !!projects ? (
+          projects.length
+       ) : (
+          'loading projects...'
+       )
+      }
       </div>
     );
+  }
+
+  componentDidMount() {
+    fetch('/projects')
+      .then(res => res.json())
+      .then(json => {
+        this.setState({
+          projects: json
+        })
+      })
   }
 }
 
